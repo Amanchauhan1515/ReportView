24209520fa4ad5d67fae03089f58d7e35024634f
poonam <pnmchauhan99@gmail.com>
2025-05-20 02:37:24 +0530

    Add project files.

---
+++ ReportView/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Reporting;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ReportView
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        String ConnectionString = @"Data Source=LAPTOP-D86A2RRO\SQLEXPRESS;Initial Catalog=DemoReport;Integrated Security=True;";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnClick_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            SqlCommand cmd =new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "PR_STUDENT_SELECTALL";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            conn.Close();
+
+            rv.LocalReport.DataSources.Add(new ReportDataSource("DataSet1",dt));
+            rv.LocalReport.ReportPath = Server.MapPath("~/Report/Report.rdlc");
+            rv.LocalReport.EnableHyperlinks = true;
+
+        }
+    }
+}
